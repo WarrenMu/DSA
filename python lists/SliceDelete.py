5d3a0c320b9f670a219e35f023a91b0b0fcd9a31
WarrenMu <omnikkotow@gmail.com>
2023-02-18 00:05:37 +0300

    continuation of slice and deletion

--- python lists/SliceDelete.py
+++ python lists/SliceDelete.py
@@ -8,3 +8,30 @@ print("-------------------------------------")
 myList[0:2]=['x','y']
 print(myList[:])
 print("-------------------------------------")
+
+# we can delete using
+#   -pop() //deletes the last element without a given value
+#   -remove()
+#   -del
+
+myList.pop()
+print(myList)
+print("-------------------------------------")
+myList.pop(4)
+print(myList)
+print("-------------------------------------")
+
+del myList[2]
+print(myList)
+print("-------------------------------------")
+
+myList.remove('x')
+print(myList)
+print("-------------------------------------")
+
+
+
+
+
+
+
