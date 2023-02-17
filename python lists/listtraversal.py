c6b2701d6348facdcb2a994e31d0c0b7c799a5b1
WarrenMu <omnikkotow@gmail.com>
2023-02-17 23:39:30 +0300

    traversal of lists

--- python lists/listtraversal.py
+++ python lists/listtraversal.py
@@ -1,4 +1,9 @@
 ShoppingLIst=['milk','cheese','butter']
 
-for value in ShoppingLIst:
-    print(value)
+# for value in ShoppingLIst:
+#     print(value)
+
+for i in range(len(ShoppingLIst)):
+    ShoppingLIst[i]=ShoppingLIst[i]+"+"
+    print(ShoppingLIst[i])
+    
